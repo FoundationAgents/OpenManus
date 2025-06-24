c466855fbb7aac93e6873ad64ca4a8f42ff72885
iheytang <dehui1012@qq.com>
2025-06-25 00:44:05 +0800

    feat: init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="welcome">
+      <img src="/logo.jpg" alt="OpenManus" className="logo" />
+      <div>No fortress, purely open ground. OpenManus is Coming.</div>
+    </div>
+  );
+}
+
+export default App;
