76e941282f80b0ef8c8bf0615db9a0f61ca57f4a
HansleCho <whgkstmf@yonsei.ac.kr>
2025-07-27 22:39:12 +0900

    revert

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,7 +2,6 @@ class ToolError(Exception):
     """Raised when a tool encounters an error."""
 
     def __init__(self, message):
-        super().__init__(message)
         self.message = message
 
 
