cb9a9fca9ba34a35a68582a92cd2dda9e675dd16
polaris <147257063+Aapolaris@users.noreply.github.com>
2025-11-12 11:59:53 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-subprocess.run([
-    "D:\\nodejs\\npx.cmd",
-    "-y",
-    "@modelcontextprotocol/server-filesystem",
-    "D:\\PersonalLearning\\20251023_OpenManus\\OpenManus\\workspace"
-])
-
